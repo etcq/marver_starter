0dc15d5c957ec608e8baaddf1da655d72548c334
Anton Sushnikov <resonans93@gmail.com>
2022-05-06 18:18:40 +0500

    charlist complete

---
+++ src/components/charList/CharList.js
@@ -0,0 +1,88 @@
+import { Component } from 'react/cjs/react.production.min';
+import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import './charList.scss';
+
+class CharList extends Component {
+
+    state = {
+        chars: [],
+        loading: true,
+        error: false
+    }
+
+
+    marvelService = new MarvelService();
+
+    onCharLoaded = (chars) => {
+        this.setState({
+            chars,
+            loading: false
+        })
+    }
+
+    updateCharacters = () => {
+        this.marvelService
+            .getAllCharacters()
+            .then(this.onCharLoaded)
+    }
+
+    componentDidMount() {
+        this.marvelService
+        .getAllCharacters()
+        .then(this.onCharLoaded)
+        .catch(this.onError)
+    }
+
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    renderItems(arr) {
+        const charCard = arr.map(({id, name, thumbnail}) => {
+            const objFit = thumbnail.includes('image_not_available');
+            return (
+                <li className="char__item char__item_selected" key={id}>
+                    <img src={thumbnail} alt={name} style={objFit ? {objectFit: "contain"} : null}/>
+                    <div className="char__name">{name}</div>
+                </li>
+            )
+        })
+        return (
+            <ul className="char__grid">
+                {charCard}
+            </ul>
+        )
+
+    }
+    
+    render () {
+        const {chars, loading, error} = this.state;
+
+        const items = this.renderItems(chars);
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
+
+        return (
+
+            <div className="char__list">
+                {errorMessage}
+                {spinner}
+                {content}
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+
+
+}
+
+export default CharList;
